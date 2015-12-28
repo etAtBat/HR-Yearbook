a46c3916da481bee556d297aec005b30ebe8d2fb
Egan Tamashiro <egantamashiro@gmail.com>
2015-12-28 11:06:20 -0800

    commit to make yearbook repo up-to-date

--- client/js/views/StudentsView.js
+++ client/js/views/StudentsView.js
@@ -3,7 +3,6 @@ var StudentsView = Backbone.View.extend({
   el: $('#page-content-container'),
 
   initialize: function() {
-    // console.log('the other side ',this.collection);
     this.render();
   },
 
@@ -11,8 +10,7 @@ var StudentsView = Backbone.View.extend({
     var prepareForDOM = this.collection.models.map(function(model){
        return new StudentEntryView({model: Student}).render(model);
     }.bind(this))
-    debugger
-    this.$el.html(prepareForDOM.join(''));
+    this.$el.html(prepareForDOM);
   }
 
 });
